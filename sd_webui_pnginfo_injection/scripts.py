ed6e5de0f45925c52cc15d48b688ed661350ceb0
bluelovers <codelovers@users.sourceforge.net>
2024-06-27 01:34:59 +0800

    fix: title

--- sd_webui_pnginfo_injection/scripts.py
+++ sd_webui_pnginfo_injection/scripts.py
@@ -2,7 +2,7 @@ import modules.scripts as scripts
 from modules import script_callbacks
 from modules.script_callbacks import ImageSaveParams
 
-from sd_webui_pnginfo_injection.logger import my_print
+from sd_webui_pnginfo_injection.logger import my_print, extension_label
 from sd_webui_pnginfo_injection.on_before_image_saved import add_resource_hashes, _add_resource_hashes_core_dict
 from sd_webui_pnginfo_injection.utils_webui import get_grid_from_res
 
@@ -15,6 +15,9 @@ class Script(scripts.Script):
         """
 
         script_callbacks.on_before_image_saved(self.before_image_saved)
+
+    def title(self):
+        return extension_label
 
     def before_image_saved(self, image_save_params: ImageSaveParams):
         add_resource_hashes(image_save_params)
