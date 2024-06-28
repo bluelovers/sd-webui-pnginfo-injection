78305682cea8941272a2e0a0e7c7cba2dbc2ce5f
bluelovers <codelovers@users.sourceforge.net>
2024-06-29 04:34:46 +0800

    chore: sort hashes

--- tests/test-01.py
+++ tests/test-01.py
@@ -29,12 +29,14 @@ class MyTestCase(unittest.TestCase):
         res, resource_hashes, hashes_is_changed = _add_resource_hashes_core_params(obj)
         self.assertEqual(resource_hashes, {"vae": "df3c506e51", "embed:Tower13_Gizelle": "9ece223c52", "embed:EasyNegative": "c74b4e810b", "model": "0a880e98ab"})
 
+        print(json.dumps(resource_hashes, indent=2))
+
         res["Hashes"] = json.dumps(resource_hashes)
         parameters = dict_to_infotext(res)
 
         self.maxDiff = None
 
-        self.assertEqual(obj.pnginfo.parameters, parameters.strip('\u200b\n'))
+        self.assertEqual("Steps: 20, Sampler: DPM++ 2M, Schedule type: Karras, CFG scale: 7, Seed: 557152044, Size: 768x512, Model hash: 0a880e98ab, Model: meinacetusorionmix_v10, VAE hash: df3c506e51, VAE: kl-f8-anime2.ckpt, Hypertile U-Net: True, Hypertile VAE: True, ADetailer model: hand_yolov8n.pt, ADetailer confidence 2nd: 0.3, ADetailer dilate erode 2nd: 4, ADetailer mask blur 2nd: 4, ADetailer denoising strength 2nd: 0.4, ADetailer inpaint only masked 2nd: True, ADetailer inpaint padding 2nd: 32, ADetailer ControlNet model 2nd: Passthrough, ADetailer version: 24.6.0, Template Seeds: 557152044, Template Seeds Sub: 1298678300, TI hashes: \"Tower13_Gizelle: 9ece223c52f8, EasyNegative: c74b4e810b03\", Hardware Info: \"RTX 3060 Ti 8GB, i7-13700, 32GB RAM\", Time taken: 22.5 sec., Version: v1.9.4-191-g6ca0466e, Hashes: {\"model\": \"0a880e98ab\", \"vae\": \"df3c506e51\", \"embed:Tower13_Gizelle\": \"9ece223c52\", \"embed:EasyNegative\": \"c74b4e810b\"}", parameters.strip('\u200b\n'))
         self.assertIn('embed:EasyNegative', parameters)
 
         obj2 = lambda: None
