af92ba4e0c7e33be68df11b8a88d84ddc18a220d
bluelovers <codelovers@users.sourceforge.net>
2024-06-29 15:43:43 +0800

    Update bundle_hashes.py

--- sd_webui_pnginfo_injection/bundle_hashes.py
+++ sd_webui_pnginfo_injection/bundle_hashes.py
@@ -1,7 +1,7 @@
 
 bundle_hashes = {
-    # https://civitai.com/models/481009?modelVersionId=590649
-    "C0rn_Fl4k3s": "014F70D45B",
+    # https://civitai.com/models/481009
+    "C0rn_Fl4k3s": "DCD9BC82A8",
     # https://civitai.com/models/449400?modelVersionId=558445
     "lazy-wildcards": "532F010BCC",
 }
