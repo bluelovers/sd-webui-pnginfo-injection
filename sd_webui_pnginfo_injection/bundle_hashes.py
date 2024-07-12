fcd9fb381dab23a1c2fcf55f92eea42302e80c27
bluelovers <codelovers@users.sourceforge.net>
2024-07-13 02:12:28 +0800

    chore: update hash

--- sd_webui_pnginfo_injection/bundle_hashes.py
+++ sd_webui_pnginfo_injection/bundle_hashes.py
@@ -6,6 +6,6 @@ class EnumBundleHashes(Enum):
     # https://civitai.com/models/481009
     C0rn_Fl4k3s = "DCD9BC82A8"
     # https://civitai.com/models/449400
-    lazy_wildcards = "532F010BCC"
+    lazy_wildcards = "F745906C9E"
     # https://civitai.com/models/138970
     Billions_of_Wildcards = "462B82E7EF"
