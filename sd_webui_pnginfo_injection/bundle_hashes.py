0237330f1cb62e7b964b33e9a0e4f6f3fb34be53
bluelovers <codelovers@users.sourceforge.net>
2024-07-17 19:41:25 +0800

    fix: hashes

--- sd_webui_pnginfo_injection/bundle_hashes.py
+++ sd_webui_pnginfo_injection/bundle_hashes.py
@@ -4,8 +4,8 @@ from enum import Enum, unique
 @unique
 class EnumBundleHashes(Enum):
     # https://civitai.com/models/481009
-    C0rn_Fl4k3s = "DCD9BC82A8"
+    C0rn_Fl4k3s = "F745906C9E"
     # https://civitai.com/models/449400
-    lazy_wildcards = "F745906C9E"
+    lazy_wildcards = "532F010BCC"
     # https://civitai.com/models/138970
     Billions_of_Wildcards = "462B82E7EF"
