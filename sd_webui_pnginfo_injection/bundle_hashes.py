2973f407d1478f3bd94baaf18f6a86b75ffcee95
GitHub Action <action@github.com>
2024-08-01 08:35:52 +0000

    build: update bundle_hashes.py

--- sd_webui_pnginfo_injection/bundle_hashes.py
+++ sd_webui_pnginfo_injection/bundle_hashes.py
@@ -4,7 +4,7 @@ from enum import Enum, unique
 @unique
 class EnumBundleHashes(Enum):
     # https://civitai.com/models/481009
-    C0rn_Fl4k3s = "CDD1242794"
+    C0rn_Fl4k3s = "E5C633CC29"
     # https://civitai.com/models/449400
     lazy_wildcards = "532F010BCC"
     # https://civitai.com/models/138970
