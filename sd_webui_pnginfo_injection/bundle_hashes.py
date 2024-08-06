4ca13b1eb16b1060c680b51cda4e8d43e7f6d8da
GitHub Action <action@github.com>
2024-08-06 11:11:05 +0000

    build: update bundle_hashes.py

--- sd_webui_pnginfo_injection/bundle_hashes.py
+++ sd_webui_pnginfo_injection/bundle_hashes.py
@@ -6,6 +6,6 @@ class EnumBundleHashes(Enum):
     # https://civitai.com/models/481009
     C0rn_Fl4k3s = "E5C633CC29"
     # https://civitai.com/models/449400
-    lazy_wildcards = "532F010BCC"
+    lazy_wildcards = "EE2C671AA8"
     # https://civitai.com/models/138970
     Billions_of_Wildcards = "462B82E7EF"
