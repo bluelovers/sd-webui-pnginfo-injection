7b5e7255e9e995b7a81e4a6bdef78524a229cdf7
bluelovers <codelovers@users.sourceforge.net>
2024-10-08 22:05:30 +0800

    .

--- sd_webui_pnginfo_injection/bundle_hashes.py
+++ sd_webui_pnginfo_injection/bundle_hashes.py
@@ -5,9 +5,12 @@ from enum import Enum, unique
 class EnumBundleHashes(Enum):
     # https://civitai.com/models/481009
     C0rn_Fl4k3s = "E5C633CC29"
+
     # https://civitai.com/models/449400
     lazy_wildcards = "028BB2A421"
+
     # https://civitai.com/models/138970
     Billions_of_Wildcards = "A872958EED"
+
     # https://civitai.com/models/272654/wildcards-vault?modelVersionId=317312
     navi_atlas = "2910F6A456"
